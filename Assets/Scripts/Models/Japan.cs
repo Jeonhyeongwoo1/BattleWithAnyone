03ee5b2a31e2399635749d787ab225b8c250a3e8
Jeon <jeonhw@a2tec.co.kr>
2022-01-17 17:59:49 +0900

    맵 구현 1차 작업중   - Japan, Scifi, Suburban, WildWest 기본형태만 구현

---
+++ Assets/Scripts/Models/Japan.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Japan : BaseModel
+{
+
+	public override void LoadModel(UnityAction done = null) { }
+	public override void UnLoadModel(UnityAction done = null) { }
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
