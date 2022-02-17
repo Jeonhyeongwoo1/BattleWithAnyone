1325fa073f46c12fd5960558e967e75b5f7a1587
Jeonhyeongwoo1 <guddn1234k@gmail.com>
2022-02-17 23:25:52 +0900

    디버깅 로깅에서 warning 생성되는 스크립트 주석처리

--- Assets/Packages/Photon/PhotonChat/Demos/Common/EventSystemSpawner.cs
+++ Assets/Packages/Photon/PhotonChat/Demos/Common/EventSystemSpawner.cs
@@ -9,7 +9,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Photon.Chat.UtilityScripts
 {
@@ -21,20 +20,21 @@ namespace Photon.Chat.UtilityScripts
     {
         void OnEnable()
         {
-            #if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
             Debug.LogError("PUN Demos are not compatible with the New Input System, unless you enable \"Both\" in: Edit > Project Settings > Player > Active Input Handling. Pausing App.");
             Debug.Break();
             return;
-            #endif
+#endif
+            /*
+                        EventSystem sceneEventSystem = FindObjectOfType<EventSystem>();
+                        if (sceneEventSystem == null)
+                        {
+                            GameObject eventSystem = new GameObject("EventSystem");
 
-            EventSystem sceneEventSystem = FindObjectOfType<EventSystem>();
-            if (sceneEventSystem == null)
-            {
-                GameObject eventSystem = new GameObject("EventSystem");
-
-                eventSystem.AddComponent<EventSystem>();
-                eventSystem.AddComponent<StandaloneInputModule>();
-            }
+                            eventSystem.AddComponent<EventSystem>();
+                            eventSystem.AddComponent<StandaloneInputModule>();
+                        }
+                        */
         }
     }
 }
