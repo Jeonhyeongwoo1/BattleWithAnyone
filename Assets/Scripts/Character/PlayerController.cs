b1315fcd7c60a5c49b8ae3efa947515141705d90
Jeonhyeongwoo1 <guddn1234k@gmail.com>
2022-02-22 19:51:51 +0900

    캐릭터 Animation 1차작업(Jump, Move, Reload, Attack 구현)

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -5,67 +5,132 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public CharacterController character;
+	public CharacterController character;
 
-    [SerializeField] float m_JumpForce = 1f;
-    [SerializeField] float m_MoveSpeed = 1f;
-    [SerializeField] float m_MaxDistance = 0.5f;
+	[SerializeField] Animator m_Animator;
+	[SerializeField] float m_JumpForce;
+	[SerializeField] float m_MoveSpeed;
+	[SerializeField] float m_MaxDistance = 0.5f;
+	[SerializeField] float m_AnimationSmoothTime = 0.1f;
+    [SerializeField] float m_AnimationPlayTransition = 0.15f;
 
-    float m_Gravitiy = 9.8f;
-    PlayerActionsScript m_PlayerActions;
+	float m_Gravitiy = 9.8f;
+	PlayerActionsScript m_PlayerActions;
+	Vector3 dirY = Vector3.zero;
+	Vector2 m_CurrentAnimationBlendVector;
+	Vector2 m_AnimationVelocity;
 
-    void Awake()
-    {
-        m_PlayerActions = new PlayerActionsScript();
-    }
+	bool m_IsAttacking = false;
+	bool m_IsRolling = false;
 
-    void OnEnable()
-    {
-        m_PlayerActions.Player.Jump.started += Jump;
-        m_PlayerActions.Player.Enable();
-    }
+    int jumpAnimation; //Jump 애니메이션 이름이 포함된다
+    Vector2 m_Direction;
 
-    void OnDisable()
-    {
-        m_PlayerActions.Player.Jump.started -= Jump;
-        m_PlayerActions.Player.Disable();
-    }
-
-    private void Update()
-    {
-        Vector2 v = m_PlayerActions.Player.Move.ReadValue<Vector2>();
-
-        if (!IsGrounded())
-        {
-            dirY += Vector3.down * m_Gravitiy * Time.deltaTime;
+	void Jump(InputAction.CallbackContext obj)
+	{
+		if (IsGrounded())
+		{
+			m_Animator.CrossFade(jumpAnimation, m_AnimationPlayTransition);
+			dirY = Vector3.up * m_JumpForce;
+			Vector2 v = m_PlayerActions.Player.Move.ReadValue<Vector2>();
+            m_Direction = v;
         }
-Debug.Log(character.isGrounded);
+	}
 
-        character.Move(v.x * m_MoveSpeed * transform.right + v.y * transform.forward * m_MoveSpeed + dirY);
-    }
-    
-    Vector3 dirY = Vector3.zero;
-    
-    void Jump(InputAction.CallbackContext obj)
-    {
-        if (IsGrounded())
-        {
-            dirY = Vector3.up * m_JumpForce;
+	void Attack(InputAction.CallbackContext obj)
+	{
+		m_IsAttacking = !m_IsAttacking;
+
+		if (m_IsAttacking)
+		{
+			StartCoroutine(Attacking());
+		}
+
+	}
+
+	void Reload(InputAction.CallbackContext obj)
+	{
+		Debug.Log("Reload");
+		m_Animator.SetTrigger("Reload");
+	}
+
+	void Rolling(InputAction.CallbackContext obj)
+	{
+		if (m_IsRolling) { return; }
+		m_IsRolling = true;
+
+
+	}
+
+	IEnumerator Attacking()
+	{
+		m_Animator.SetBool("Attack", true);
+		while (m_IsAttacking)
+		{
+			yield return null;
+		}
+
+		m_Animator.SetBool("Attack", false);
+	}
+
+	bool IsGrounded()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, m_MaxDistance))
+			return true;
+		else
+			return false;
+	}
+
+	private void Update()
+	{
+		Vector2 v = m_PlayerActions.Player.Move.ReadValue<Vector2>();
+		m_CurrentAnimationBlendVector = v != Vector2.zero ? Vector2.SmoothDamp(m_CurrentAnimationBlendVector, v, ref m_AnimationVelocity, m_AnimationSmoothTime) : Vector2.zero;
+
+		bool isGround = IsGrounded();
+
+		if (!isGround || !character.isGrounded)
+		{
+			dirY += Vector3.down * m_Gravitiy * Time.deltaTime;
+            v = m_Direction;
         }
-    }
 
-    bool IsGrounded()
-    {
-        Debug.DrawRay(transform.position, Vector3.down * m_MaxDistance, Color.blue);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, m_MaxDistance))
-            return true;    
-        else
-            return false;
-    }
+		m_Animator.SetFloat("Horizontal", isGround ? m_CurrentAnimationBlendVector.x : 0);
+		m_Animator.SetFloat("Vertical", isGround ? m_CurrentAnimationBlendVector.y : 0);
+		character.Move((v.x * m_MoveSpeed * transform.right + v.y * transform.forward * m_MoveSpeed + dirY) * Time.deltaTime);
 
-    private void OnDrawGizmos() {
+	}
 
-    }
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void LateUpdate()
+	{
+	}
+
+	void Awake()
+	{
+		m_PlayerActions = new PlayerActionsScript();
+	}
+
+	void OnEnable()
+	{
+		m_PlayerActions.Player.Jump.started += Jump;
+		m_PlayerActions.Player.Attack.started += Attack;
+		m_PlayerActions.Player.Reload.started += Reload;
+		m_PlayerActions.Player.Rolling.started += Rolling;
+		m_PlayerActions.Player.Enable();
+
+        jumpAnimation = Animator.StringToHash("Jump");
+	}
+
+	void OnDisable()
+	{
+		m_PlayerActions.Player.Jump.started -= Jump;
+		m_PlayerActions.Player.Attack.started -= Attack;
+		m_PlayerActions.Player.Reload.started -= Reload;
+		m_PlayerActions.Player.Rolling.started -= Rolling;
+		m_PlayerActions.Player.Disable();
+	}
 
 }
